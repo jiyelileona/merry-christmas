6be1fc884e07bf39c64d4107dc68385f6a8aaa1c
Jiye Li <15832778446@163.com>
2020-12-24 18:03:50 -0600

    add speech feature

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const main = document.querySelector('.main');
 const video = document.querySelector('video');
+const button = document.querySelector('button');
 
 const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene({
@@ -23,3 +24,32 @@ setInterval(function () {
 
   video.currentTime = delay;
 }, 100);
+
+const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'en-US';
+
+recognition.onresult = function (e) {
+  let transcript = e.results[0][0].transcript;
+  getResponse(transcript);
+};
+
+recognition.onspeechend = function () {
+  recognition.stop();
+};
+
+button.addEventListener('click', function () {
+  recognition.start();
+});
+
+const getResponse = transcript => {
+  const response = new SpeechSynthesisUtterance();
+  response.text = 'ho ho ho';
+  response.volume = 1;
+
+  if (transcript.includes('Merry Christmas')) {
+    response.text = 'Merry Christmas to you too baby.';
+  }
+
+  window.speechSynthesis.speak(response);
+};
